2f30abf580a4e26c2b2a1ffbfa003e0d5ad8a234
Brton-7 <brandon.tong7235@gmail.com>
2022-08-04 16:57:15 -0700

    add C# assignment 3

---
+++ C# Assignment 3 pt 1/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Class1
+    {
+        public int[] GenerateNumbers(int num)
+        {
+            int[] temp = new int[num];
+            for(int i = 0; i < num; i++)
+            {
+                temp[i] = i + 1;
+            }
+            return temp;
+        }
+        public void PrintNumbers(int[] nums)
+        {
+            string temp = "";
+            for (int i = 0; i < nums.Length; i++)
+            {
+                temp += nums[i] + " ";
+            }
+            Console.WriteLine("Your array: " + temp);
+        }
+        public void Reverse(int[] nums)
+        {
+            int temp;
+
+            for(int i = 0; i < nums.Length/2; i++)
+            {
+                temp = nums[i];
+                nums[i] = nums[nums.Length - i - 1];
+                nums[nums.Length - i - 1] = temp;
+            }
+        }
+        public void fibonacci(int num)
+        {
+            string temp = "";
+            int[] fib = new int[num];
+            fib[0] = fib[1] = 1;
+            for(int i = 0; i < num; i++)
+            {
+                if(i > 1)
+                {
+                    fib[i] = fib[i - 1] + fib[i - 2];
+                }
+                temp += fib[i] + " ";
+            }
+            Console.WriteLine("fibonacci sequence up to " + num + ": " + temp);
+        }
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Class1 methods = new Class1();
+
+
+            int[] numbers = methods.GenerateNumbers(num);
+            methods.Reverse(numbers);
+            methods.PrintNumbers(numbers);
+
+            methods.fibonacci(num);
+        }
+    }
+}
---
+++ C# Assignment 3 pt 1/Program.cs
@@ -0,0 +1,47 @@
+﻿//Assignment 03 Object-Oriented Programming
+//1. a-Public: public access members/modifiers that can be used anywhere in a project
+//   b-Private: private access membeer that can only be accessed in current class
+//   c-Protected: protected access member can be accessed within current class and derived classes
+//   d-Internal: member that can be accessed by classes within current project assembly
+//   e-Protected Internal: members that can only be accessed by classes within projects or child classes in other projects
+//   f-Private Protected: Members that can be accessed by current class or child class within a project assembly only
+
+//2. The static keyword declares a stic member that applies to its type, the const keyword declares a constant that cannot
+//   be modified. The read only keyword defines an unchanging field that can be assigned at compile time or runtime(in constructor).
+
+//3. A constructor is used to create objects for a class and assigns values to its members
+
+//4. Partial keyword is useful when implemeting the functionality of a single class across multiple files
+
+//5. A tupple is a data structure that can store elements of multiple data types
+
+//6. A tuple is a reference type that can be used in place of classes and structs
+
+//7. Overloading is having multiple methods with the same name but with different parameters and implementations.
+//   Overidding is redefining the implementation of a method in an inherited class.
+
+//8. A field is a variable of certain type declared in a class and a property is a member of a class that requires other class members 
+//   to read, write or access this 
+
+//9. A method parameter can be made optional by either adding a default value or overloading the method.
+
+//10. The difference between interface and abstract is that abstract is used as a base of a class and cannot be instantiated but accessed.
+//    An interface is different in that its members must be instantiated by the class that is using it.
+
+//11. Members of an interface can have the same access modifiers as regular classes
+
+//12. True
+//13. True
+//14. False
+//15. False
+//16. True
+//17. True
+//18. True
+//19. False
+//20. False
+//21. True
+//22. True
+//23. True
+
+
+
